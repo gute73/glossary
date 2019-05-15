cb14ffcab74ccb3b0f4b87cc503e954742a2d8f2
Mark D. King <kingmarkd@gmail.com>
2019-05-14 23:53:50 -0400

    Database config

--- app/database.php
+++ app/database.php
@@ -5,11 +5,11 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 
 $capsule->addConnection([
+  'host' => 'us-cdbr-iron-east-02.cleardb.net',
   'driver' => 'mysql',
-  'host' => 'localhost',
-  'username' => 'root',
-  'password' => '',
-  'database' => 'glossary_db',
+  'username' => 'b8d38bd5114bd2',
+  'password' => '476728ec',
+  'database' => 'heroku_83e6aec7b812598',
   'charset' => 'utf8',
   'prefix' => ''
 ]);
